2e389f5c182caa486680bd12105d3300f1286211
dynae <nightcorebanksy@gmail.com>
2024-09-23 15:04:50 -0300

    completed up to Pickups, all working properly

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update() {
+        // Rotate the object on X, Y, and Z axes by specified amounts, adjusted for frame rate.
+        transform.Rotate(new Vector3(0, 0, 180) * Time.deltaTime);
+    }
+
+}
