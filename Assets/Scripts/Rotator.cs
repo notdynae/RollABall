cd19a0555dff757a46c47fbec1cd42dff69698c7
dynae <nightcorebanksy@gmail.com>
2024-09-23 15:27:58 -0300

    rotating enemies added, needs collision checks and death functions

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour {
 
+    public Vector3 rotate;
     // Update is called once per frame
     void Update() {
         // Rotate the object on X, Y, and Z axes by specified amounts, adjusted for frame rate.
-        transform.Rotate(new Vector3(0, 0, 180) * Time.deltaTime);
+        transform.Rotate(rotate * Time.deltaTime);
     }
 
 }
